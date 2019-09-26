cfa9de3c205f5392e422a885f7aeb27f4f95c2e9
Romane G <romane71193@gmail.com>
2019-09-26 09:04:49 -0400

    created database

--- dlptooling/dlptooling/settings.py
+++ dlptooling/dlptooling/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
         'USER': 'root',
-        'PASSWORD': 'rkgrkgrkg',
+        'PASSWORD': '',
     }
 }
 # Password validation
